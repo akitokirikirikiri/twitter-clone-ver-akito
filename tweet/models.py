bc7c93fe167e8bd563a674f8a39dd4ee1ed8d937
akitokirikirikiri <piqurusuuuu@gmail.com>
2021-04-24 06:39:44 +0900

    first commit

---
+++ tweet/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.content
+    
+    
+    #データベースを作ってくれる
+# Userに様々な情報が管理されている
+# Create your models here.
---
+++ tweet/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import ListView
+from .models import Post #同じフォルダという意味で.を加えている
+
+class PostListView(ListView):
+    model = Post
+    template_name = "tweet/index.html"
+    context_object_name = 'posts'
+    ordering = ('-date_posted')
+    paginate_by = 5
+
+# Create your views here.
